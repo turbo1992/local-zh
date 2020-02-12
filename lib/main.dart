4015546f741edbd974bcaa9e8cfaaa7f6cde28ad
turbo <turbo1992@163.com>
2020-02-12 17:40:27 +0800

    feat(flutter): + android studio build

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class ZhiHu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "知乎-高仿版",
+      title: "知乎-s高仿版",
       home: new Index(),
     );
   }
