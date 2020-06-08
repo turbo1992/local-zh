654a4f92069275c4af6d0860bb212faf4029e0e8
turbo <turbo1992@163.com>
2020-06-08 14:58:19 +0800

    default color config

--- lib/global_config.dart
+++ lib/global_config.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class GlobalConfig {
-  static bool dark = true;
-  static ThemeData themeData = new ThemeData.dark();
-  static Color searchBackgroundColor = Colors.white10;
-  static Color cardBackgroundColor = new Color(0xFF222222);
-  static Color fontColor = Colors.white30;
+//  static bool dark = true;
+//  static ThemeData themeData = new ThemeData.dark();
+//  static Color searchBackgroundColor = Colors.white10;
+//  static Color cardBackgroundColor = new Color(0xFF222222);
+//  static Color fontColor = Colors.white30;
+  static bool dark = false;
+  static ThemeData themeData = new ThemeData(
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: new Color(0xFFEBEBEB),
+  );
+  static Color searchBackgroundColor = new Color(0xFFEBEBEB);
+  static Color cardBackgroundColor = Colors.white;
+  static Color fontColor = Colors.black54;
 }
